8507763975e4ddde869d911d01d087aaedad93e5
KimSungJun <jt2302@naver.com>
2026-09-23 12:38:44 +0900

    0011

--- src/main/java/com/practice/boundedContext/post/app/PostFacade.java
+++ src/main/java/com/practice/boundedContext/post/app/PostFacade.java
@@ -3,6 +3,7 @@ package com.practice.boundedContext.post.app;
 import com.practice.boundedContext.member.domain.Member;
 import com.practice.boundedContext.post.domain.Post;
 import com.practice.boundedContext.post.out.PostRepository;
+import com.practice.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,7 +22,7 @@ public class PostFacade {
     }
 
     @Transactional
-    public Post write(Member author, String title, String content) {
+    public RsData<Post> write(Member author, String title, String content) {
         return postWriteUseCase.write(author, title, content);
     }
 
--- src/main/java/com/practice/boundedContext/post/app/PostWriteUseCase.java
+++ src/main/java/com/practice/boundedContext/post/app/PostWriteUseCase.java
@@ -4,6 +4,7 @@ import com.practice.boundedContext.member.domain.Member;
 import com.practice.boundedContext.post.domain.Post;
 import com.practice.boundedContext.post.out.PostRepository;
 import com.practice.global.eventPublisher.EventPublisher;
+import com.practice.global.rsData.RsData;
 import com.practice.shared.post.dto.PostDto;
 import com.practice.shared.post.event.PostCreatedEvent;
 import lombok.RequiredArgsConstructor;
@@ -17,11 +18,11 @@ public class PostWriteUseCase {
     private final PostRepository postRepository;
     private final EventPublisher eventPublisher;
 
-    public Post write(Member author, String title, String content) {
+    public RsData<Post> write(Member author, String title, String content) {
         Post post = postRepository.save(new Post(author, title, content));
 
         eventPublisher.publish(new PostCreatedEvent(new PostDto(post)));
 
-        return post;
+        return new RsData<>("201-1", "%번 글이 생성되었습니다.".formatted(post.getId()), post);
     }
 }
