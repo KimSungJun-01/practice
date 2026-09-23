6d3c36e5ec58afbf99f19763cca0cab5d5c383db
KimSungJun <jt2302@naver.com>
2026-09-23 14:33:55 +0900

    0013

--- src/main/java/com/practice/boundedContext/member/app/MemberFacade.java
+++ src/main/java/com/practice/boundedContext/member/app/MemberFacade.java
@@ -1,6 +1,7 @@
 package com.practice.boundedContext.member.app;
 
 import com.practice.boundedContext.member.domain.Member;
+import com.practice.boundedContext.member.domain.MemberPolicy;
 import com.practice.global.exception.DomainException;
 import com.practice.boundedContext.member.out.MemberRepository;
 import com.practice.global.rsData.RsData;
@@ -15,6 +16,7 @@ import java.util.Optional;
 public class MemberFacade {
     private final MemberRepository memberRepository;
     private final MemberJoinUseCase memberJoinUseCase;
+    private final MemberPolicy memberPolicy;
 
     @Transactional(readOnly = true)
     public long count() {
@@ -34,5 +36,9 @@ public class MemberFacade {
     @Transactional(readOnly = true)
     public Optional<Member> findById(int id) {
         return memberRepository.findById(id);
+    }
+
+    public String getRandomSecureTip() {
+        return "비밀번호의 유효기간은 %d일 입니다.".formatted(memberPolicy.getNeedToChangePasswordDays());
     }
 }
--- src/main/java/com/practice/boundedContext/post/app/PostWriteUseCase.java
+++ src/main/java/com/practice/boundedContext/post/app/PostWriteUseCase.java
@@ -1,5 +1,6 @@
 package com.practice.boundedContext.post.app;
 
+import com.practice.boundedContext.member.app.MemberFacade;
 import com.practice.boundedContext.member.domain.Member;
 import com.practice.boundedContext.post.domain.Post;
 import com.practice.boundedContext.post.out.PostRepository;
@@ -17,12 +18,18 @@ import java.util.Optional;
 public class PostWriteUseCase {
     private final PostRepository postRepository;
     private final EventPublisher eventPublisher;
+    private final MemberFacade memberFacade;
 
     public RsData<Post> write(Member author, String title, String content) {
         Post post = postRepository.save(new Post(author, title, content));
 
         eventPublisher.publish(new PostCreatedEvent(new PostDto(post)));
 
-        return new RsData<>("201-1", "%번 글이 생성되었습니다.".formatted(post.getId()), post);
+        String randomSecureTip = memberFacade.getRandomSecureTip();
+        return new RsData<>(
+                "201-1",
+                "%번 글이 생성되었습니다. 보안 팁 : %s".formatted(post.getId(), randomSecureTip),
+                post
+        );
     }
 }
