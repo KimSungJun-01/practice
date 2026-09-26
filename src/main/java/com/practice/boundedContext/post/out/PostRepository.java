e41c7a343466ab26e4198be18dee88f921be457b
KimSungJun <jt2302@naver.com>
2026-09-26 11:43:54 +0900

    0031

--- src/main/java/com/practice/boundedContext/post/out/PostRepository.java
+++ src/main/java/com/practice/boundedContext/post/out/PostRepository.java
@@ -3,5 +3,8 @@ package com.practice.boundedContext.post.out;
 import com.practice.boundedContext.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Integer> {
+    List<Post> findByOrderByIdDesc();
 }
